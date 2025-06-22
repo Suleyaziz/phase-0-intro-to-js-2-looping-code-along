3211698f8c10ea53c0a5bbd294ba38773e16a2c7
Suleiman Aziz <Suleimanaziz212@gmail.com>
2025-06-22 20:57:59 +0300

    Labs Completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solutions in this file
-function writeCards(names) {
 const thankYouMessages = [];
+
+function writeCards(names) {
   for (let i = 0; i < names.length; i++) {
     thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
   }
